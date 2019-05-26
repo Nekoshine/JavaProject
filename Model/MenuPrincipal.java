e789d942f554df4a9c738407ba49f367360ee6e3
Alzaphano <50359182+Alzaphano@users.noreply.github.com>
2019-05-26 11:51:59 +0200

    Amélioration menu principal
    Le code est moche mais np on verra plus tard
    probleme quand la musique se lance on ne peut rien faire on doit attendre la fin

--- Model/MenuPrincipal.java
+++ Model/MenuPrincipal.java
@@ -1,8 +1,25 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
 import java.awt.GridLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -11,7 +28,10 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+
 public class MenuPrincipal /*extends JFrame implements ActionListener*/{
+	
+	
 
 	public MenuPrincipal() {
 
@@ -29,19 +49,25 @@ public class MenuPrincipal /*extends JFrame implements ActionListener*/{
 		
 		JPanel boutons = new JPanel(new GridLayout(4, 1));
 		
+		Font f = new Font("Bernard MT Condensed",Font.PLAIN,20);
+		
 		JButton bclient = new JButton("Client");
+		bclient.setFont(f);
 		bclient.setPreferredSize(new Dimension(150,100));
 		JPanel tc = new JPanel();
 		tc.add(bclient);
 		JButton bloc = new JButton("Locations");
+		bloc.setFont(f);
 		bloc.setPreferredSize(new Dimension(150,100));
 		JPanel tl = new JPanel();
 		tl.add(bloc);
 		JButton bres = new JButton("Restitutions");
+		bres.setFont(f);
 		bres.setPreferredSize(new Dimension(150,100));
 		JPanel tr = new JPanel();
 		tr.add(bres);
 		JButton bgestion = new JButton("Gestion");
+		bgestion.setFont(f);
 		bgestion.setPreferredSize(new Dimension(150,100));
 		JPanel tg = new JPanel();
 		tg.setBackground(Color.white);
@@ -55,8 +81,11 @@ public class MenuPrincipal /*extends JFrame implements ActionListener*/{
 		boutons.add(tg);
 		fenetre.add(boutons,BorderLayout.CENTER);
 		
+		
+		
 		Icon icon1 = new ImageIcon("giphy.gif");
         JLabel label1 = new JLabel(icon1);
+        label1.setPreferredSize(new Dimension(550,550));
         JPanel pani1 = new JPanel();
         pani1.add(label1);
         pani1.setBackground(Color.white);
@@ -66,6 +95,103 @@ public class MenuPrincipal /*extends JFrame implements ActionListener*/{
         JPanel pani2 = new JPanel();
         pani2.add(label2);
         pani2.setBackground(Color.white);
+        pani2.addMouseListener(new MouseListener() {
+        	private AudioFormat format;
+            private byte[] samples;
+            /**
+             * 
+             * @param filename le lien vers le fichier song (URL ou absolute path)
+             */
+            public void sound(String filename){
+             try{
+              AudioInputStream stream = AudioSystem.getAudioInputStream(new File(filename));
+              format = stream.getFormat();
+              samples = getSamples(stream);
+             }
+             catch (UnsupportedAudioFileException e){
+              e.printStackTrace();
+             }
+             catch (IOException e){
+              e.printStackTrace();
+             }
+            }
+            public byte[] getSamples(){
+             return samples;
+            }
+            public byte[] getSamples(AudioInputStream stream){
+             int length = (int)(stream.getFrameLength() * format.getFrameSize());
+             byte[] samples = new byte[length];
+             DataInputStream in = new DataInputStream(stream);
+             try{
+              in.readFully(samples);
+             }
+             catch (IOException e){
+              e.printStackTrace();
+             }
+             return samples;
+            }
+            public void play(InputStream source) {
+             int bufferSize = format.getFrameSize() * Math.round(format.getSampleRate() / 10);
+             byte[] buffer = new byte[bufferSize];
+             SourceDataLine line;
+             try{
+              DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+              line = (SourceDataLine)AudioSystem.getLine(info);
+              line.open(format, bufferSize);
+             }
+             catch (LineUnavailableException e){
+              e.printStackTrace();
+              return;
+             }
+             line.start();
+             try{
+              int numBytesRead = 0;
+              while (numBytesRead != -1){
+               numBytesRead = source.read(buffer, 0, buffer.length);
+               if (numBytesRead != -1)
+                line.write(buffer, 0, numBytesRead);
+              }
+             }
+             catch (IOException e){
+             e.printStackTrace();
+             }
+             line.drain();
+             line.close();
+            }
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				System.out.println("Bonjour");
+				/*this.sound("dejavu.WAV");
+			    InputStream stream = new ByteArrayInputStream(this.getSamples()); 
+			    this.play(stream);*/
+				
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
         fenetre.add(pani2,BorderLayout.SOUTH);
         Icon icon3 = new ImageIcon("moto.gif");
         JLabel label3 = new JLabel(icon3);
@@ -80,7 +206,10 @@ public class MenuPrincipal /*extends JFrame implements ActionListener*/{
 		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
+	
+	
 	public static void main(String[] args) {
 		MenuPrincipal menu = new MenuPrincipal();
+		System.out.println(Arrays.toString(GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames()));
 	}
 }
